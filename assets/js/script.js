bd601323724f94063d75a494a086c435d371c89b
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-04-15 17:10:36 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// 1JQkv9QuItbSZdek9-2UfH6pqzA
+const API_KEY = "1JQkv9QuItbSZdek9-2UfH6pqzA";
+const API_URL = "https://ci-jshint.herokuapp.com.api/";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
