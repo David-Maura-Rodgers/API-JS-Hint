f9103365525564073286c480c2bbc3a2bdda5b9c
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-04-16 13:20:03 +0000

    create getStatus function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 // 1JQkv9QuItbSZdek9-2UfH6pqzA
 const API_KEY = "1JQkv9QuItbSZdek9-2UfH6pqzA";
-const API_URL = "https://ci-jshint.herokuapp.com.api/";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById('status').addEventListener('click', e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
